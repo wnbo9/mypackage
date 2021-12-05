66eef655a8b65fc5d1e634241c3e8a18eb3cde6f
wnbo9 <wnbo@umich.edu>
2021-12-05 11:52:20 -0500

    dataset

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' mt_cars
-#'
 #' Motor Trend Car Road Tests
+#'
+#' The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973–74 models).
 #'
 #' @format A data frame with 32 observations on 11 (numeric) variables.
 #' \describe{
