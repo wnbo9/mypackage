2a735deb94d625191ea6381068961a61071ad753
wnbo9 <wnbo@umich.edu>
2021-12-05 11:26:50 -0500

    test changes

--- tests/testthat/test-lm_fit.R
+++ tests/testthat/test-lm_fit.R
@@ -4,5 +4,6 @@ test_that("lm_fit works", {
 
   expect_equal(class(lm_fit(mpg ~ cyl, dat)), "list")
   expect_equal(lm_fit(mpg ~ cyl, dat)$p, 2)
+  expect_equal(lm_fit(mpg ~ cyl + disp - 1, dat)$p, 2)
   expect_equal(lm_fit(mpg ~ cyl + disp, dat[1:2,]), -1)
 })
