d8901fb5edabf01dbdf71f6b5ee4bfdae976b179
wnbo9 <wnbo@umich.edu>
2021-12-05 11:43:50 -0500

    changes for dataset documentation

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' mt_cars
+#'
+#' Motor Trend Car Road Tests
+#'
+#' @format A data frame with 32 observations on 11 (numeric) variables.
+#' \describe{
+#'   \item{mpg}{Miles/(US) gallon}
+#'   \item{cyl}{Number of cylinders}
+#'   \item{disp}{Displacement (cu.in.)}
+#'   \item{hp}{Gross horsepower}
+#'   \item{drat}{Rear axle ratio}
+#'   \item{wt}{Weight (1000 lbs)}
+#'   \item{qsec}{1/4 mile time}
+#'   \item{vs}{Engine (0 = V-shaped, 1 = straight)}
+#'   \item{am}{Transmission (0 = automatic, 1 = manual)}
+#'   \item{gear}{Number of forward gears}
+#'   \item{carb}{Number of carburetors}
+#'   ...
+#' }
+"mt_cars"
