fc6a6faf7eb0fb80a2a921414d820490391ba890
wnbo9 <wnbo@umich.edu>
2021-12-05 11:12:45 -0500

    test changes for lm_anova_fit

--- tests/testthat/test-lm_anova_fit.R
+++ tests/testthat/test-lm_anova_fit.R
@@ -5,5 +5,5 @@ test_that("lm_anova_fit works", {
   colnames(xx) <- c("outcome", "x1", "x2")
   bx <- as.matrix(xx[,1:2])
 
-  expect_equal(round(lm_anova_fit(bx), 6), -0.219496)
+  expect_equal(as.numeric(round(lm_anova_fit(bx), 6)), -0.219496)
 })
